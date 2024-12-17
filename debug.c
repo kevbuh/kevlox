c2496ce89392c574cbc0e139dc11ffefac8e0acd
Kevin Buhler <buhlerkw@gmail.com>
2024-12-16 19:20:54 -0700

    disassembler line info

--- debug.c
+++ debug.c
@@ -3,7 +3,7 @@
 #include "value.h"
 
 void disassembleChunk(Chunk* chunk, const char* name) {
-    printf("== %s ==\n", name);
+    printf("CNK  LN  OP ===%s=== I VAL\n", name);
 
     for (int offset = 0; offset < chunk->count;) {
         offset = disassembleInstruction(chunk, offset);
@@ -25,6 +25,12 @@ static int constantInstruction(const char* name, Chunk* chunk, int offset) {
 
 int disassembleInstruction(Chunk* chunk, int offset) {
     printf("%04d", offset); // prints byte offset of the given instruction
+
+    if (offset > 0 && chunk->lines[offset] == chunk->lines[offset-1]) {
+        printf("   ^");
+    } else {
+        printf("%4d ", chunk->lines[offset]);
+    }
 
     uint8_t instruction = chunk->code[offset]; // opcode
     switch(instruction) {
