4f9f8cf0a3fed6ed20a69c1ccbdbb87020472c87
Kevin Buhler <buhlerkw@gmail.com>
2024-12-16 19:52:51 -0700

    instruction run()ner

--- vm.c
+++ vm.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "common.h"
 #include "vm.h"
 
@@ -5,6 +6,38 @@ VM vm; // TODO: don't make this global
 
 void initVM() {
 
+}
+
+static InterpretResult run() {
+    #define READ_BYTE() (*vm.ip++)
+    #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+
+    for (;;) {
+        uint8_t instruction;
+        switch (instruction = READ_BYTE()) {
+            case OP_CONSTANT: {
+                Value constant = READ_CONSTANT();
+                printValue(constant);
+                printf("\n");
+                break;
+            }
+            case OP_RETURN: {
+                return INTERPRET_OK;
+            }
+        }
+    }
+
+    #undef READ_BYTE
+    #undef READ_CONSTANT
+}
+
+InterpretResult interpret(Chunk* chunk) {
+    vm.chunk = chunk;
+
+    // TODO: store as local variable so its in a register
+    // first byte of code in the chunk
+    vm.ip = vm.chunk->code;
+    return run();
 }
 
 void freeVM() {
