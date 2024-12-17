099e35c8fe5bde00c9bec95ab5ab27d23437acdd
Kevin Buhler <buhlerkw@gmail.com>
2024-12-16 18:58:15 -0700

    add line number

--- chunk.h
+++ chunk.h
@@ -13,12 +13,13 @@ typedef struct {
     int count;
     int capacity;
     uint8_t* code; // array of bytes
+    int* lines;
     ValueArray constants;
 } Chunk; // Bytecode is a series of instructions
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
 int addConstant(Chunk* chunk, Value value);
 
 #endif
