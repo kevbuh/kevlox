ca26dc2c1c02dce5fd50caf3ade58ee83a58764a
Kevin Buhler <buhlerkw@gmail.com>
2024-12-22 00:57:35 -0700

    EQUAL, GREATER, LESS

--- clox/debug.c
+++ clox/debug.c
@@ -11,6 +11,7 @@ void disassembleChunk(Chunk* chunk, const char* name) {
     }
 }
 
+// print instruction and return next byte
 static int simpleInstruction(const char* name, int offset) {
     printf("%s\n", name);
     return offset + 1; // return next byte
@@ -36,13 +37,19 @@ int disassembleInstruction(Chunk* chunk, int offset) {
     uint8_t instruction = chunk->code[offset]; // opcode
     switch(instruction) {
         case OP_CONSTANT: return constantInstruction("OP_CONSTANT", chunk, offset);
-        case OP_ADD: return simpleInstruction("OP_ADD", offset);
+        case OP_NIL:      return simpleInstruction("OP_NIL", offset);
+        case OP_TRUE:     return simpleInstruction("OP_TRUE", offset);
+        case OP_FALSE:    return simpleInstruction("OP_FALSE", offset);
+        case OP_EQUAL:    return simpleInstruction("OP_EQUAL", offset);
+        case OP_GREATER:  return simpleInstruction("OP_GREATER", offset);
+        case OP_LESS:     return simpleInstruction("OP_LESS", offset);
+        case OP_ADD:      return simpleInstruction("OP_ADD", offset);
         case OP_SUBTRACT: return simpleInstruction("OP_SUBTRACT", offset);
         case OP_MULTIPLY: return simpleInstruction("OP_MULTIPLY", offset);
-        case OP_DIVIDE: return simpleInstruction("OP_DIVIDE", offset);
-        case OP_NOT: return simpleInstruction("OP_NOT", offset);
-        case OP_NEGATE: return simpleInstruction("OP_NEGATE", offset);
-        case OP_RETURN: return simpleInstruction("OP_RETURN", offset);
+        case OP_DIVIDE:   return simpleInstruction("OP_DIVIDE", offset);
+        case OP_NOT:      return simpleInstruction("OP_NOT", offset);
+        case OP_NEGATE:   return simpleInstruction("OP_NEGATE", offset);
+        case OP_RETURN:   return simpleInstruction("OP_RETURN", offset);
         default: 
             printf("Unknown opcode %d \n", instruction);
             return offset + 1;
