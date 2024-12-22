ea722854005ecc8bbf799935ac5efcb2918772b3
Kevin Buhler <buhlerkw@gmail.com>
2024-12-22 12:57:33 -0700

    print string

--- clox/object.h
+++ clox/object.h
@@ -6,9 +6,7 @@
 
 // extract the object type tag from given value
 #define OBJ_TYPE(value)  (AS_OBJ(value)->type)
-
 #define IS_STRING(value) isObjType(value, OBJ_STRING)
-
 #define AS_STRING(value) ((ObjString*)AS_OBJ(value))
 #define AS_CSTRING(value) (((ObjString*)AS_OBJ(value))->chars)
 
@@ -28,6 +26,7 @@ struct ObjString {
 };
 
 ObjString* copyString(const char* chars, int length);
+void printObject(Value value);
 
 static inline bool isObjType(Value value, ObjType type) {
     return IS_OBJ(value) && AS_OBJ(value)->type == type;
